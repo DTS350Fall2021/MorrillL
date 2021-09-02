85a07a233c50f0488500e22e38824036d52ef911
logan1m <loganjmorrill@gmail.com>
2021-09-02 16:42:30 -0500

    Done

--- Week2/Task3/task3.R
+++ Week2/Task3/task3.R
@@ -6,13 +6,14 @@ str(iris)
 
 head(iris)
 str(iris)
-
+#1
 ggplot(data = iris) +
-  geom_point(mapping = aes(x = Sepal.Length, y = Sepal.Width, color = Species))
+  geom_point(mapping = aes(x = Sepal.Length, y = Sepal.Width, color = Species, shape = Species))
 
 # The Setosa iris species has a shorter sepal length and a longer sepal width
 # versus the Versicolor iris and the Virginica iris.
 
+#2
 ggplot(data = iris) +
   geom_point(mapping = aes(x = Petal.Width, y = Petal.Length, 
                            color = Species, shape = Species)) +
@@ -21,11 +22,13 @@ ggplot(data = iris) +
 #The Virginica iris has the biggest petal length and width.
 #The Setosa iris has the smallest petal length and width.
 
+#3
 ggplot(data = iris, aes(x = Petal.Length, y = Petal.Width)) +
   geom_point( aes(color = Species, shape = Species)) +
   geom_smooth()
 #Graph 3 shows there is a positive correlation between petal length and petal width.
 
+#4
 ggplot(data = iris, aes(x = Sepal.Length, fill = Species)) +
   geom_bar(color = 'black', stat = 'bin', bins = 19) +
   geom_vline(xintercept = mean(iris[["Sepal.Length"]]), linetype = "dotted")
