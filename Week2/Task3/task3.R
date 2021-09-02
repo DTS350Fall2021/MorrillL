7d2c44522835c005bfa1f16e2bb1d7e61a60b15a
logan1m <loganjmorrill@gmail.com>
2021-09-02 14:22:15 -0500

    task 3

---
+++ Week2/Task3/task3.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+dim(iris)
+str(iris)
+?iris
+
+head(iris)
+summary(iris)
+
+ggplot(data = iris) +
+  geom_point(mapping = aes(x = Sepal.Length, y = Sepal.Width, color = Species))
+
+ggplot(data = iris) +
+  geom_point(mapping = aes(x = Petal.Width, y = Petal.Length, 
+                           color = Species, shape = Species)) +
+  facet_wrap(~Species) +
+  theme(legend.position = 'none')
