bf69991401c3af5d359d45d6d127cec65ceaac2f
logan1m <loganjmorrill@gmail.com>
2021-09-02 16:38:57 -0500

    Done

--- Week2/CaseStudy2/coralData.R
+++ Week2/CaseStudy2/coralData.R
@@ -2,3 +2,6 @@ library(tidyverse)
 
 urlfile = "https://github.com/WJC-Data-Science/DTS350/raw/master/coral.csv"
 mydata <- read_csv(url(urlfile))
+
+str(mydata)
+head(mydata)
--- Week2/Task3/task3.R
+++ Week2/Task3/task3.R
@@ -10,16 +10,25 @@ str(iris)
 ggplot(data = iris) +
   geom_point(mapping = aes(x = Sepal.Length, y = Sepal.Width, color = Species))
 
+# The Setosa iris species has a shorter sepal length and a longer sepal width
+# versus the Versicolor iris and the Virginica iris.
+
 ggplot(data = iris) +
   geom_point(mapping = aes(x = Petal.Width, y = Petal.Length, 
                            color = Species, shape = Species)) +
   facet_wrap(~Species) +
   theme(legend.position = 'none')
+#The Virginica iris has the biggest petal length and width.
+#The Setosa iris has the smallest petal length and width.
 
 ggplot(data = iris, aes(x = Petal.Length, y = Petal.Width)) +
   geom_point( aes(color = Species, shape = Species)) +
   geom_smooth()
+#Graph 3 shows there is a positive correlation between petal length and petal width.
 
 ggplot(data = iris, aes(x = Sepal.Length, fill = Species)) +
   geom_bar(color = 'black', stat = 'bin', bins = 19) +
   geom_vline(xintercept = mean(iris[["Sepal.Length"]]), linetype = "dotted")
+#Setosa irises seem to be on the low end of the average sepal lengths,
+#virginica seem to be on the higher end of the average sepal lengths, and
+#versicolor seem to be around the average of the sepal lengths.
